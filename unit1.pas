95e4aad037dbd15fc3cc28f4813ed9fdfe565cf0
Steve Schilz <steve.schilz@gmail.com>
2023-11-17 17:57:11 -0800

    Add pascal components to display all database data in grids - Works

--- unit1.pas
+++ unit1.pas
@@ -5,10 +5,27 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, DBGrids, SQLDB,
+  DB, SQLite3Conn;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    DBGrid1: TDBGrid;
+    DBGrid2: TDBGrid;
+    DBGridPeople: TDBGrid;
+    DSPeople: TDataSource;
+    DSPhones: TDataSource;
+    DSTypes: TDataSource;
+    MainMenu1: TMainMenu;
+    SQLite3Connection1: TSQLite3Connection;
+    QueryPeople: TSQLQuery;
+    QueryPhones: TSQLQuery;
+    QueryTypes: TSQLQuery;
+    SQLTransaction1: TSQLTransaction;
+    procedure SQLite3Connection1AfterConnect(Sender: TObject);
   private
 
   public
@@ -21,6 +38,13 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.SQLite3Connection1AfterConnect(Sender: TObject);
+begin
+
+end;
 
 end.
 
