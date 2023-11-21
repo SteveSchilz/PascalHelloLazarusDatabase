95f54b654310ddfcc5f4b5fe2d1fd718618b64cf
Steve Schilz <steve.schilz@gmail.com>
2023-11-21 12:04:15 -0800

    Add EditSearch field with Search and Add Buttons  Working Filter on Search ID

--- unit1.pas
+++ unit1.pas
@@ -5,26 +5,31 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, DBGrids, SQLDB,
-  DB, SQLite3Conn;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, DBGrids,
+  StdCtrls, SQLDB, DB, SQLite3Conn;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    ButtonSearch: TButton;
+    ButtonAdd: TButton;
     DBGrid1: TDBGrid;
     DBGrid2: TDBGrid;
     DBGridPeople: TDBGrid;
     DSPeople: TDataSource;
     DSPhones: TDataSource;
     DSTypes: TDataSource;
+    EditSearch: TEdit;
+    LabelContact: TLabel;
     MainMenu1: TMainMenu;
     SQLite3Connection1: TSQLite3Connection;
     QueryPeople: TSQLQuery;
     QueryPhones: TSQLQuery;
     QueryTypes: TSQLQuery;
     SQLTransaction1: TSQLTransaction;
+    procedure ButtonSearchClick(Sender: TObject);
     procedure SQLite3Connection1AfterConnect(Sender: TObject);
   private
 
@@ -44,6 +49,32 @@ implementation
 procedure TForm1.SQLite3Connection1AfterConnect(Sender: TObject);
 begin
 
+end;
+
+procedure TForm1.ButtonSearchClick(Sender: TObject);
+var
+   FilterString : String;
+begin
+     (*
+      * String escaping and concatenation:
+      *   * double quotes escapes a quote:  '' => ",
+      *   * % is a normal character
+      *   * plus sign concatenates elements
+      * This ends up something like
+      *
+      *      'First Like "%Ste%"'
+      *)
+     FilterString := 'First Like ''%' + EditSearch.Text + '%''';
+     WriteLn(FilterString);
+
+     (* Server Filter filters on the database side, as opposed to .Filter which
+      * Filters the result table
+      *)
+     QueryPeople.ServerFilter := FilterString;
+     QueryPeople.ServerFiltered := true;
+
+     QueryPeople.refresh();
+     DBGridPeople.refresh();
 end;
 
 end.
